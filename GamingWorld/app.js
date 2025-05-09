6e5ae87805322efb450f520b1130d2b36fbb7ac2
unknown <rohitratate855@gmail.com>
2025-05-09 11:58:29 +0530

    custom cursor

--- GamingWorld/app.js
+++ GamingWorld/app.js
@@ -1,13 +1,40 @@
-const nextButton = document.querySelector('.next-btn');
-const video = document.querySelector('.hero-video');
+const nextButton = document.querySelector(".next-btn");
+const video = document.querySelector(".hero-video");
 
-const movieList = ['videos/hero-1.mp4','videos/hero-2.mp4','videos/hero-3.mp4','videos/hero-4.mp4'];
+const movieList = [
+  "videos/hero-1.mp4",
+  "videos/hero-2.mp4",
+  "videos/hero-3.mp4",
+  "videos/hero-4.mp4",
+];
 
 let index = 0;
-nextButton.addEventListener('click',function(){
-    index++;
-    video.src = movieList[index];
-    if(index === 3){
-        index = -1;
-    }
-})
+nextButton.addEventListener("click", function () {
+  index++;
+  video.src = movieList[index];
+  if (index === 3) {
+    index = -1;
+  }
+});
+
+
+// Custom Cursor -----------------
+const cursor = document.querySelector(".cursor");
+var timeout;
+//follow cursor on mousemove
+document.addEventListener("mousemove", (e) => {
+cursor.style.left = `${e.clientX}px`;
+cursor.style.top = `${e.clientY}px`;
+  cursor.style.display = "block";
+
+  //   cursor effect on mousestop
+  function mouseStopped() {
+    cursor.style.display = "none";
+  }
+  clearInterval(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
